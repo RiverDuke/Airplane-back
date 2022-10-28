e5aede7e9fdbb6b2d6eb8cb56b291a911de1d912
River <oasisnile556@gmail.com>
2022-10-28 16:58:00 -0400

    attempt connection

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(
+  cors({
+    origin: "",
+  })
+);
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
