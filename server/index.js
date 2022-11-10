d2ed47a05986ca4611156a60321552baff12785e
River <oasisnile556@gmail.com>
2022-11-09 22:29:22 -0500

    2

--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
-const path = require("path");
+// const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+// let origin1;
+
+// if (process.env.NODE_ENV === "development") {
+//   console.log("hello");
+//   origin1 = "localhost:3000";
+// } else {
+//   origin1 = process.env.ALLOWED_ORIGIN;
+// }
+
+// console.log("origin1:", origin1);
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://airplane-front.uk.r.appspot.com/",
+    origin: [process.env.ALLOWED_ORIGIN, "http://localhost:3000"],
   })
 );
 
