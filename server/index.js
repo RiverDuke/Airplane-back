52c0420b97fb0bcfc5bf224097c8ac08154a897a
River <oasisnile556@gmail.com>
2022-12-01 15:43:05 -0500

    session

--- server/index.js
+++ server/index.js
@@ -1,26 +1,49 @@
 // const path = require("path");
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
 const knex = require("./db/connection");
 const loginRouter = require("./login/login.router");
-require("dotenv").config();
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-console.log("testing:", process.env.TEST);
-app.use(express.json());
-
-let corsOptions = {
+const corsOptions = {
   origin: ["https://airplane-front.uk.r.appspot.com", "http://localhost:3000"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
+const sessionOptions = {
+  secret: "some secret",
+  resave: false,
+  saveUninitialized: true,
+  store: new knexSessionStore({
+    knex: require("./db/connection.js"),
+    tablename: "sessions",
+    // sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+};
 
+app.use(express.json());
 app.use(cors(corsOptions));
+
+app.use(session(sessionOptions));
 
 app.get("/api", async (req, res) => {
   const data = await knex("testing").select("*");
-  // const info = await data.json();
+
+  if (req.session.viewCount) {
+    req.session.viewCount++;
+  } else {
+    req.session.viewCount = 1;
+  }
+
   console.log(data);
+  console.log(req.session.viewCount);
 
   res.json({
     data,
